f9fa2edce4279734bef37f856c131935e8eb162f
mvalenciaz <mvalenciaz@misena.edu.co>
2022-02-03 15:35:51 -0500

    Funciones Multiples

---
+++ php/07-seleccionmultiple.php
@@ -0,0 +1,16 @@
+<?php
+$i = 1;
+switch ($i) {
+    case '1':
+        echo "Dia lunes <br>";
+        break;
+        case '2':
+            echo "Dia Martes <br>";
+            break;
+            case '3':
+                echo "Dia Miercoles <br>";
+                break;
+    default:
+        echo "Un dia del fin se semana";
+        break;
+}
