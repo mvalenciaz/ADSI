e10e658908d429157d7ee1b1d05d4c2f29aade33
mvalenciaz <mvalenciaz@misena.edu.co>
2022-02-06 18:55:29 -0500

    Agregando el menú

--- php/crud/index.php
+++ php/crud/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,7 +8,10 @@
     <title>CRUD</title>
     <link rel="stylesheet" href="./css/bootstrap.min.css">
 </head>
+
 <body>
+    <?php include_once "./views/menu.php" ?>
     <script src="./js//bootstrap.min.js"></script>
 </body>
+
 </html>
