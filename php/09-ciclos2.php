6e974f545c54dda5373eab5c8b170406647cb988
mvalenciaz <mvalenciaz@misena.edu.co>
2022-02-07 10:15:37 -0500

    Ciclos

--- php/09-ciclos2.php
+++ php/09-ciclos2.php
@@ -1,5 +1,5 @@
 <?php
 $nombres = ['Victor', 'Iris', 'Elizabeth', 'Mauro']; //definicion del arreglo
-foreach ($nombres as $indice => $nombre) {
-    echo $nombre. "<br>". "Se encontraba en el indice $indice <br>";
+foreach ($nombres as $indice => $nombre) { //Se define variable para identificar la posicion en el arreglo
+    echo $nombre. " Se encontraba en el indice $indice <br>";
 }
